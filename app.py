acc48f59e99f6617b918d98f863cd1e97f61c119
JoaquinAntunez <joaquin.antunezuribe@gmail.com>
2026-03-19 23:52:18 -0300

    Avance hasta el slide

--- app.py
+++ app.py
@@ -4,18 +4,19 @@ import base64
 
 
 img_logo = 'img/logo4.png'
+img_imagen = 'img/logo_front.png'
 
 # Crear navegación con columnas (alternativa más simple)
 
-
+st.logo(img_logo, size='large', link='http://localhost:8501/')
 
 page = None
 with st.container():
     col1, col2, col3, col4, col5 = st.columns(5)
 
     with col1:
-        #st.logo(img_logo, size='large', link='http://localhost:8501/')
-        st.image(img_logo, width=150)
+        #st.logo(img_logo, size='small', link='http://localhost:8501/')
+        st.image(img_imagen, width=100)
 
     with col2:
         if st.button("Inicio", use_container_width=True):
@@ -68,7 +69,7 @@ st.markdown("---")
 #    st.write("Información de contacto")
 
 
-with open("video/video.mp4", "rb") as f:
+with open("video/video2.mp4", "rb") as f:
     video_bytes = f.read()
 
 video_base64 = base64.b64encode(video_bytes).decode()
@@ -76,7 +77,6 @@ video_base64 = base64.b64encode(video_bytes).decode()
 #css nav_bar
 st.markdown(f"""
 <style>
-"[![]{img_logo}](http://localhost:8501/)"
 
 .video-container {{
     position: relative;
@@ -129,6 +129,8 @@ st.markdown(f"""
     width: 80%;
 }}
 
+
+
 </style>
 
 <div class="video-container">
@@ -167,3 +169,98 @@ with st.container():
         st.write("Capturamos el comportamiento en espacios físicos (casa piloto y obra) a través de dispositivos inteligentes de alta precisión, entregando datos accionables en tiempo real que permiten optimizar ventas, eficientizar operaciones y mejorar la experiencia de compra y avance en obra.")
     with col2:
         st.image("img/Terreno.png")
+
+# funcion para tener imagen en el slider
+def img_as_data_uri(path):
+    ext = path.split(".")[-1]
+    data = base64.b64encode(open(path, "rb").read()).decode("utf-8")
+    return f"data:image/{ext};base64,{data}"
+
+img_uri = img_as_data_uri("img/imagen1.png")
+
+#silder
+st.markdown(f"""
+<style>
+.slider {{
+  position: relative;
+  width: 100%;
+  height: 250px;
+  overflow: hidden;
+  border-radius: 16px;
+}}
+
+/* Slides */
+.slide {{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  animation: fade 15s infinite;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 60px;
+  box-sizing: border-box;
+  color: black;
+}}
+
+/* Fondo */
+.slide:nth-child(1) {{ background: #fffdfa; animation-delay: 0s;}}
+.slide:nth-child(2) {{ background: #fffdfa; animation-delay: 5s;}}
+.slide:nth-child(3) {{ background: #fffdfa; animation-delay: 10s;}}
+
+/* Contenido */
+.text {{ width: 50%; }}
+.text h2 {{ font-size: 36px; }}
+.text p {{ font-size: 18px; opacity: 0.8; }}
+
+.image {{ width: 40%; }}
+.image img {{
+  width: 100%;ß
+  border-radius: 12px;
+}}
+
+/* Animación fade */
+@keyframes fade {{
+  0% {{ opacity: 0; }}
+  5% {{ opacity: 1; }}
+  30% {{ opacity: 1; }}
+  35% {{ opacity: 0; }}
+  100% {{ opacity: 0; }}
+}}
+</style>
+
+<div class="slider">
+
+  <div class="slide">
+    <div class="text">
+      <h2>IoT en terreno</h2>
+      <p>Captura en tiempo real lo que ocurre en casas piloto y obra.</p>
+    </div>
+    <div class="image">
+      <img src= {img_uri}>
+    </div>
+  </div>
+
+  <div class="slide">
+    <div class="text">
+      <h2>AI que analiza</h2>
+      <p>Transforma comportamiento en decisiones inteligentes.</p>
+    </div>
+    <div class="image">
+      <img src="img/imagen1.png">
+    </div>
+  </div>
+
+  <div class="slide">
+    <div class="text">
+      <h2>Plataforma SaaS</h2>
+      <p>Visualiza toda tu operación en una sola interfaz.</p>
+    </div>
+    <div class="image">
+      <img src="https://images.unsplash.com/photo-1551288049-bebda4e38f71">
+    </div>
+  </div>
+
+</div>
+""", unsafe_allow_html=True)
