8b64a3ca991359fee9cd24844bf9880d21ec4931
JoaquinAntunez <joaquin.antunezuribe@gmail.com>
2026-03-31 23:39:43 -0300

    actualización31-03

--- app_dos.py
+++ app_dos.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import base64
+import urllib.parse
 
 
 
@@ -42,6 +43,10 @@ with st.container():
 
 
 st.markdown("---")
+#muestra los iconos de fontawesome
+st.markdown("""
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+""", unsafe_allow_html=True)
 
 # Mostrar contenido según la página
 if page == "Inicio":
@@ -61,7 +66,7 @@ if page == "Inicio":
 
     .video-container video {{
          width: 100%;
-         opacity: 0.3;
+         opacity: 0.5;
          border-radius: 10px;
     }}
 
@@ -74,7 +79,7 @@ if page == "Inicio":
          left: 50%;
          transform: translate(-50%, -50%);
          color: black;
-         font-size: 24px;
+         font-size: clamp(16px, 2vw, 22px);
           text-align: center;
          font-weight: bold;
     }}
@@ -85,7 +90,7 @@ if page == "Inicio":
          left: 50%;
          transform: translate(-50%, -50%);
          color: black;
-         font-size: 40px;
+         font-size: clamp(24px, 2.5vw, 42px);
          font-weight: bold;
          text-align: center;
          width: 80%;
@@ -93,7 +98,7 @@ if page == "Inicio":
     }}
 
     .subtitle {{
-         font-size: 1.5vw;
+         font-size: 1.2vw;
          position: absolute;
          top: 75%;
          left: 50%;
@@ -133,7 +138,7 @@ if page == "Inicio":
         col1, = st.columns(1, gap="large")
         with col1:
             if st.button("Revisa nuestras soluciones", use_container_width=True):
-                st.session_state.page = "Contacto"
+                st.session_state.page = "Soluciones"
                 st.rerun()
 
     st.markdown("---")
@@ -244,9 +249,9 @@ if page == "Inicio":
         data = base64.b64encode(open(path, "rb").read()).decode("utf-8")
         return f"data:image/{ext};base64,{data}"
 
-    img_uri = img_as_data_uri("img/texto3.png")
-    img_uri2 = img_as_data_uri("img/Texto4.png")
-    img_uri3 = img_as_data_uri("img/imagen_dash.png")
+    img_uri = img_as_data_uri("img/texto8.png")
+    img_uri2 = img_as_data_uri("img/Texto6.png")
+    img_uri3 = img_as_data_uri("img/Texto7.png")
 
     #silder
     st.markdown(f"""
@@ -350,56 +355,195 @@ if page == "Inicio":
     with col1:
         st.subheader("Benchmark online", text_alignment="justify")
         st.caption('Ve más allá de tu proyecto, analiza el mercado y acúa a tiempo ')
-        st.button("Mas información", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"), key="benchmark",)
+        with st.popover("Mas información"):
+            st.subheader("Benchmark online", text_alignment="justify")
+            st.write("El benchmark online es una herramienta que permite comparar el desempeño de tu proyecto inmobiliario con otros proyectos similares en el mercado. A través de datos en tiempo real, puedes analizar tendencias, identificar oportunidades y tomar decisiones informadas para optimizar tus ventas y operaciones.")
+            st.button("Agenda una asesoría", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"), key="benchmark")
     with col2:
         st.subheader("Gestión de conversión", text_alignment="left")
         st.caption('Encuentra oportunidades para aumentar las  utilidades de tu proyecto')
-        st.button("Mas información", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"), key="conversion")
+        with st.popover("Mas información"):
+            st.subheader("Gestión de conversión", text_alignment="justify")
+            st.write("La gestión de conversión es un proceso que se enfoca en optimizar cada etapa del embudo de ventas para convertir más prospectos en clientes. Utilizando datos en tiempo real, puedes identificar puntos de fricción, mejorar la experiencia del cliente y aumentar las tasas de conversión, lo que se traduce en mayores utilidades para tu proyecto inmobiliario.")
+            st.button("Agenda una asesoría", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"), key="conversion")
     with col3:
         st.subheader("In-proyect Tracking", text_alignment="justify")
         st.caption('Convierte cada interacción en una oportunidad de venta o eficiencia operativa')
-        st.button("Mas información", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"), key="obra")
+        with st.popover("Mas información"):
+            st.subheader("In-proyect Tracking", text_alignment="justify")
+            st.write("El In-project Tracking es una solución que permite monitorear en tiempo real el comportamiento de los usuarios dentro de tu proyecto inmobiliario. A través de sensores y dispositivos inteligentes, puedes capturar datos sobre cómo los prospectos interactúan con los espacios, lo que te brinda insights valiosos para mejorar la experiencia del cliente, optimizar operaciones y aumentar las oportunidades de venta.")
+            st.button("Agenda una asesoría", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"), key="tracking")
 
-    st.button("Ver todas las soluciones", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"), key="asesoria")
+    st.button("Ver todas las soluciones", use_container_width=True, on_click=lambda: st.session_state.update(page="Soluciones"), key="asesoria")
+    st.divider()
+
+
+elif page == "Soluciones":
+    img_path = 'img/soluciones_img.png'
+    def get_image_base64(img_path):
+        with open(img_path, "rb") as f:
+            return base64.b64encode(f.read()).decode("utf-8")
+
+    st.markdown(f"""
+    <style>
+
+        .caja_transparente {{
+            background-color: rgba(255, 255, 255, 0.5);
+            position: absolute;
+            top: 65%;
+            left: 38%;
+            transform: translate(-50%, -50%);
+            padding: 20px;
+            border-radius: 12px;
+            width: 70%;
+            color: black;
+            backdrop-filter: blur(4px);
+            font-size: clamp(18px, 1.5vw, 22px);
+        }}
+
+        .imagen-container {{
+            position: relative;
+            width: 100%;
+            height: 300px;
+            overflow: hidden;
+            border-radius: 12px;
+        }}
+
+    }}
+    </style>
+
+    <div class="imagen-container">
+        <img src="data:image/png;base64,{get_image_base64(img_path)}"
+            style="width:100%; height:100%; object-fit:cover; border-radius:12px">
+        </img>
+        <div class="caja_transparente">
+            <h3>Inteligencia de datos para proyectos inmobiliarios</h3>
+            <p>Toma decisiones con información real y actualizada</p>
+    </div>
+    """, unsafe_allow_html=True)
+
+    st.title("Nuestras soluciones")
+    st.markdown('##### Transformamos datos en decisiones inmobiliarias')
+    with st.container(border=True):
+        st.markdown('Desde la captación de oportunidades hasta la evaluación de proyectos, nuestra plataforma centraliza y analiza información clave del mercado en tiempo real. Esto te permite reducir la incertidumbre, detectar oportunidades y optimizar cada decisión.', text_alignment="justify")
+
+    with st.container():
+        col1, col2, col3 = st.columns(3, gap="large", border=True)
+        with col1:
+            st.subheader("Benchmark online", text_alignment="justify")
+            st.caption('Ve más allá de tu proyecto, analiza el mercado y acúa a tiempo ')
+            with st.popover("Mas información"):
+                st.subheader("Benchmark online", text_alignment="justify")
+                st.write("El benchmark online es una herramienta que permite comparar el desempeño de tu proyecto inmobiliario con otros proyectos similares en el mercado. A través de datos en tiempo real, puedes analizar tendencias, identificar oportunidades y tomar decisiones informadas para optimizar tus ventas y operaciones.")
+                st.button("Agenda una asesoría", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"), key="benchmark2")
+        with col2:
+            st.subheader("Gestión de conversión", text_alignment="left")
+            st.caption('Encuentra oportunidades para aumentar las  utilidades de tu proyecto')
+            with st.popover("Mas información"):
+                st.subheader("Gestión de conversión", text_alignment="justify")
+                st.write("La gestión de conversión es un proceso que se enfoca en optimizar cada etapa del embudo de ventas para convertir más prospectos en clientes. Utilizando datos en tiempo real, puedes identificar puntos de fricción, mejorar la experiencia del cliente y aumentar las tasas de conversión, lo que se traduce en mayores utilidades para tu proyecto inmobiliario.")
+                st.button("Agenda una asesoría", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"), key="conversion2")
+        with col3:
+            st.subheader("In-proyect Tracking", text_alignment="justify")
+            st.caption('Convierte cada interacción en una oportunidad de venta o eficiencia operativa')
+            with st.popover("Mas información"):
+                st.subheader("In-proyect Tracking", text_alignment="justify")
+                st.write("El In-project Tracking es una solución que permite monitorear en tiempo real el comportamiento de los usuarios dentro de tu proyecto inmobiliario. A través de sensores y dispositivos inteligentes, puedes capturar datos sobre cómo los prospectos interactúan con los espacios, lo que te brinda insights valiosos para mejorar la experiencia del cliente, optimizar operaciones y aumentar las oportunidades de venta.")
+                st.button("Agenda una asesoría", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"), key="tracking2")
+    with st.container():
+        col1, col2, col3 = st.columns(3, gap="large", border=True)
+        with col1:
+            st.subheader("Conteo de personas", text_alignment="justify")
+            st.caption('Monitorea el flujo de personas en tu proyecto, en tiempo real')
+            with st.popover("Mas información"):
+                st.subheader("Conteo de personas", text_alignment="justify")
+                st.write("El conteo de personas es una solución que permite monitorear en tiempo real el flujo de personas dentro de tu proyecto inmobiliario. A través de sensores inteligentes, puedes obtener datos precisos sobre la cantidad de visitantes, su comportamiento y patrones de movimiento, lo que te brinda insights valiosos para optimizar la experiencia del cliente, mejorar la seguridad y aumentar las oportunidades de venta.")
+                st.button("Agenda una asesoría", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"), key="conteo")
+        with col2:
+            st.subheader('Forecasting de datos')
+            st.caption('Optimiza la eficiencia de tu proyecto inmobiliario con predicciones de datos')
+            with st.popover("Mas información"):
+                st.subheader('Predicciónes de datos (forecasting)', text_alignment="justify")
+                st.write("La predicción de datos, o forecasting, es una solución que utiliza modelos analíticos avanzados para anticipar tendencias del mercado, evaluar escenarios futuros y tomar decisiones con mayor certeza. Al analizar datos históricos y en tiempo real, puedes identificar patrones y predecir comportamientos futuros, lo que te permite optimizar la eficiencia de tu proyecto inmobiliario y mantener una ventaja competitiva.")
+                st.button("Agenda una asesoría", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"), key="equipos")
+        with col3:
+            st.subheader('Gestión de zonas')
+            st.caption('Controla y optimiza el uso de espacios en tu proyecto inmobiliario')
+            with st.popover("Mas información"):
+                st.subheader('Gestión de zonas', text_alignment="justify")
+                st.write("La gestión de zonas es una solución que permite controlar y optimizar el uso de espacios en tu proyecto inmobiliario. A través de sensores y dispositivos inteligentes, puedes monitorear el uso de áreas específicas, identificar patrones de comportamiento y tomar decisiones informadas para mejorar la experiencia del cliente y maximizar el valor del proyecto.")
+                st.button("Agenda una asesoría", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"), key="zonas")
     st.divider()
 
     with st.container():
-        st.title('No reacciones por intuición, anticípate con datos', text_alignment="center")
-        st.text('Obra y piloto con datos en tiempo real para anticipar decisiones que impactan en costos, plazos y ventas', text_alignment="center")
-        st.button("Agenda una asesoría", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"))
+        st.title('Consultoría personalizada')
+        st.markdown('##### Te ayudamos a convertir datos en decisiones inteligentes')
+        col1, col2 = st.columns(2, gap="large", border=True)
+        with col1:
+            st.image("img/Texto4.png", width='content')
+            st.subheader('Tecnología IoT de alta precisión')
+            st.write('Garantizamos datos confiables con un 98% de precisión, gracias a una tecnología robusta, calibrada y monitoreada 24/7')
+        with col2:
+            st.image("img/Texto4.png", width='content')
+            st.subheader('Data accionable en tiempo real')
+            st.write('La información está disponible en tiempo real a través de nuestra plataforma, permitiéndote tomar decisiones informadas y oportunas que impactan directamente en tus resultados.')
+    with st.container():
+        col1, col2 = st.columns(2, gap="large", border=True)
+        with col1:
+            st.image("img/Texto5.png", width='content')
+            st.subheader('Acompañamiento y monitoreo continuo')
+            st.write('Nuestros especialistas están dispuestos a atender solicitudes y acompañar al cliente a transformar los datos en planes de acción concretos.')
+        with col2:
+            st.image("img/Texto5.png", width='content')
+            st.subheader('Inteligencia predictiva y toma de decisiones')
+            st.write('Anticipa tendencias del mercado, evalúa escenarios y toma decisiones con mayor certeza utilizando modelos analíticos avanzados.')
 
-    st.divider()
+    with st.container():
+        st.button("Agenda una asesoría personalizada", use_container_width=True, on_click=lambda: st.session_state.update(page="Contacto"), key="asesoria2")
 
-    with st.container(border=True, horizontal=True, horizontal_alignment="center"):
-        st.image("img/logo1.png")
-        st.markdown("Data en vivo para decisiones inmobiliarias")
-
-    st.markdown("""
-        <div style="display:flex; justify-content:center; margin-top:20px;">
-         <a href="https://instagram.com/tu_usuario" target="_blank">
-            <button style="background-color:#ffffff; color:black; border:none; padding:2px 10px; border-radius:8px; cursor:pointer; font-size:25px; margin-right:20px;"><p class="fa-brands fa-instagram"></p></button>
-         </a>
-         <a href="https://wa.me/tu_numero" target="_blank">
-            <button style="background-color:#ffffff; color:black; border:none; padding:2px 10px; border-radius:8px; cursor:pointer; font-size:25px; margin-right:20px;"><p class="fa-solid fa-phone"> </p></button>
-         </a>
-         <a href="mailto:tu_correo" target="_blank">
-            <button style="background-color:#ffffff; color:black; border:none; padding:2px 10px; border-radius:8px; cursor:pointer; font-size:25px; margin-right:20px;"><p class="fa-solid fa-envelope"> </p></button>
-         </a>
-        </div>
-                """, unsafe_allow_html=True, text_alignment="center")
-
-    st.divider()
-    st.caption("© 2026 Web TI. Todos los derechos reservados.", text_alignment="center")
-
-elif page == "Soluciones":
-    st.title("📚 Soluciones")
-    st.write("Contenido de las soluciones")
-
-    st.text_area('hola', placeholder='Escribe aqui')
 elif page == "Nosotros":
     st.title("💡 Nosotros")
     st.write("Información sobre nosotros")
 elif page == "Contacto":
-    st.title("⚙️ Contacto")
-    st.write("Información de contacto")
+    st.title("Contacto")
+    st.divider()
+    with st.container(border=True, horizontal=True, horizontal_alignment="center"):
+        st.subheader('Convierte tus datos en ventaja competitiva.', text_alignment="center")
+    st.divider()
+    with st.form("contacto", clear_on_submit=True):
+        nombre = st.text_input("Nombre", placeholder="Escribe tu nombre")
+        correo = st.text_input("Correo", placeholder="Escribe tu correo")
+        mensaje = st.text_area("Mensaje", placeholder="Escribe tu mensaje")
+        enviar = st.form_submit_button("Enviar", use_container_width=True)
+
+    if enviar:
+        if not nombre or not correo or not mensaje:
+            st.error("Por favor, completa todos los campos.")
+        else:
+            texto = f"Nombre: {nombre}\nCorreo: {correo}\nMensaje: {mensaje}"
+
+            texto_encode = urllib.parse.quote(texto)
+            numero = '56984003382'
+
+            url = f"https://api.whatsapp.com/send?phone={numero}&text={texto_encode}"
+            st.success("Redirigiendo a WhatsApp...")
+            st.link_button("WhatsApp", url=url, use_container_width=True)
+
+st.divider()
 # termina el contenido de la pagina, se puede agregar más contenido a cada sección según sea necesario
+with st.container(border=True, horizontal=True, horizontal_alignment="center"):
+        st.image("img/logo1.png")
+        st.markdown("Data en vivo para decisiones inmobiliarias", text_alignment="center")
+st.markdown("""
+    <div style="display:flex; justify-content:center; margin-top:20px;">
+            <a href="https://wa.me/tu_numero" target="_blank">
+                <button style="background-color:#ffffff; color:black; border:none; padding:2px 10px; border-radius:8px; cursor:pointer; font-size:25px; margin-right:20px;"><p class="fa-solid fa-phone"> </p></button>
+            </a>
+            <a href="mailto:tu_correo" target="_blank">
+                <button style="background-color:#ffffff; color:black; border:none; padding:2px 10px; border-radius:8px; cursor:pointer; font-size:25px; margin-right:20px;"><p class="fa-solid fa-envelope"> </p></button>
+            </a>
+    </div>
+                """, unsafe_allow_html=True, text_alignment="center")
+
+st.divider()
+st.caption("© 2026 Web TI. Todos los derechos reservados.", text_alignment="center")
